5935c7a6738c663f299461052eb2e790cfb1bf1a
leoli <leoli0913@163.com>
2021-11-29 17:23:34 +0800

    second commit

--- src/main/java/com/atguigu/git/GitDemo.java
+++ src/main/java/com/atguigu/git/GitDemo.java
@@ -8,5 +8,6 @@ package com.atguigu.git;
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
