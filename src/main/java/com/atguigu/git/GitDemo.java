3fadf38bb7f105430fc00b8348316b74b220649e
leoli <leoli0913@163.com>
2021-11-29 17:14:50 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitDemo.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @Author HP
+ * @Date: 2021-11-29 17:13
+ * @Description:
+ */
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
